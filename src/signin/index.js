70ce4218d1968ee5a2b2e58bce71167c801e171a
Antonio Paternina <acpat88@gmail.com>
2016-11-06 17:52:28 -0500

    + se añaden títulos a las páginas + con el módulo title se hace modificaciones durante navegación

--- src/signin/index.js
+++ src/signin/index.js
@@ -1,9 +1,11 @@
 var page = require('page');
 var empty = require('empty-element');
+var title = require('title');
 
 var template = require('./template');
 
 page('/signin', function(ctx, next) {
+    title('Platzigram - Signin');
     var main = empty(document.getElementById('main-container'));
     main.appendChild(template);
 });
--- src/signup/index.js
+++ src/signup/index.js
@@ -1,9 +1,11 @@
 var page = require('page');
 var empty = require('empty-element');
+var title = require('title');
 
 var template = require('./template');
 
 page('/signup', function(ctx, next) {
+    title('Platzigram - Signup');
     var main = empty(document.getElementById('main-container'));
     main.appendChild(template);
 });
