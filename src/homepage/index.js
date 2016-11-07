abe770eee7d0e288de7e574b859f3bc0b35fd42b
Antonio Paternina <acpat88@gmail.com>
2016-11-07 07:28:44 -0500

    + se avanza en creación de tarjetas del timeline

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -7,5 +7,27 @@ var template = require('./template');
 page('/', function(ctx, next) {
     title('Platzigram');
     var main = empty(document.getElementById('main-container'));
-    main.appendChild(template);
+
+    var pictures = [
+      {
+          user: {
+            username: 'acpat88',
+            avatar: 'http://www.aceshowbiz.com/images/still/avatar114.jpg'
+          },
+          url: 'office.jpg',
+          likes: 10,
+          liked: true
+      },{
+          user: {
+            username: 'fukkatsu1',
+            avatar: 'https://upload.wikimedia.org/wikipedia/commons/0/07/Avatar_girl_face.png'
+          },
+          url: 'office.jpg',
+          likes: 2,
+          liked: true
+      }
+
+    ];
+
+    main.appendChild(template(pictures));
 });
